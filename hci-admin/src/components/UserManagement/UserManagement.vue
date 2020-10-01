4d6203bc818424b01ce787a04ea262e107a36d04
Anh Dung <littlemozarthver23@gmail.com>
2020-10-02 01:03:08 +0700

    Update user management

--- hci-admin/src/components/UserManagement/UserManagement.vue
+++ hci-admin/src/components/UserManagement/UserManagement.vue
@@ -8,7 +8,7 @@
           <h1>User Management</h1>
           <el-table
             stripe
-            :data="accountData.filter(data => !search || data.fullname.toLowerCase().includes(search.toLowerCase()))"
+            :data="subAccountData"
             style="width: 95%; margin-left: 2.5%"
             empty-text="No Data">
           <el-table-column
@@ -25,23 +25,37 @@
           </el-table-column>
           <el-table-column
             align="right">
-            <template slot="header" slot-scope="scope">
+            <template slot="header">
               <el-input
                 v-model="search"
                 size="mini"
+                :change="changeTextSearch"
                 placeholder="Type to search"/>
             </template>
             <template slot-scope="scope">
               <el-button
                 size="mini"
-                @click="handleEdit(scope.$index, scope.row)">Edit</el-button>
+                style="background-color: #82FA58"
+                :disabled="scope.row.status === 'Active'"
+                @click="handleClicked(scope.$index, scope.row, 'Active')">Active</el-button>
               <el-button
                 size="mini"
                 type="danger"
-                @click="handleDelete(scope.$index, scope.row)">Delete</el-button>
+                :disabled="scope.row.status === 'Inactive'"
+                @click="handleClicked(scope.$index, scope.row, 'Inactive')">Inactive</el-button>
             </template>
           </el-table-column>
         </el-table>
+        <el-pagination
+          background
+          :page-size="pageSize"
+          :current-page.sync="currentPage"
+          style="margin-top: 10px"
+          layout="prev, pager, next"
+          :hide-on-single-page="true"
+          @current-change="changePage()"
+          :total="numOfPage * 10">
+        </el-pagination>
       </el-col>
     </el-row>
   </div>
@@ -53,23 +67,69 @@ import Menu from '../Common/Menu'
 export default {
   data () {
     return {
+      numOfPage: 0,
+      currentPage: 1,
+      pageSize: 10,
+      subAccountData: [],
+      searchList: [],
       accountData: [{
         username: 'Tom@123',
         fullname: 'Tom',
-        address: 'No. 189, Grove St, Los Angeles'
+        address: 'No. 189, Grove St, Los Angeles',
+        status: 'Active'
       }, {
         username: 'John@abc',
         fullname: 'John',
-        address: 'No. 189, Grove St, Los Angeles'
+        address: 'No. 189, Grove St, Los Angeles',
+        status: 'Inactive'
       }, {
         username: 'Morgan@xyz',
         fullname: 'Morgan',
-        address: 'No. 189, Grove St, Los Angeles'
+        address: 'No. 189, Grove St, Los Angeles',
+        status: 'Inactive'
       }, {
         username: 'Jessy@hihi',
         fullname: 'Jessy',
-        address: 'No. 189, Grove St, Los Angeles'
-      }],
+        address: 'No. 189, Grove St, Los Angeles',
+        status: 'Active'
+      }, {
+        username: 'Jerry@ccc',
+        fullname: 'Jerry',
+        address: 'No. 189, Grove St, Los Angeles',
+        status: 'Active'
+      }, {
+        username: 'Suri@sri',
+        fullname: 'Suri',
+        address: 'No. 189, Grove St, Los Angeles',
+        status: 'Active'
+      }, {
+        username: 'Mama@ma',
+        fullname: 'Mama',
+        address: 'No. 189, Grove St, Los Angeles',
+        status: 'Inactive'
+      }, {
+        username: 'Mimi@mi',
+        fullname: 'Mimi',
+        address: 'No. 189, Grove St, Los Angeles',
+        status: 'Inactive'
+      }, {
+        username: 'Chichi@chi',
+        fullname: 'Chichi',
+        address: 'No. 189, Grove St, Los Angeles',
+        status: 'Active'
+      }, {
+        username: 'Siro@sr',
+        fullname: 'Siro',
+        address: 'No. 189, Grove St, Los Angeles',
+        status: 'Active'
+      },
+      {
+        username: 'Siro@sr',
+        fullname: 'Siro',
+        address: 'No. 189, Grove St, Los Angeles',
+        status: 'Active'
+      }
+      ],
       search: ''
     }
   },
@@ -78,6 +138,8 @@ export default {
   },
   mounted () {
     this.checkAuthen()
+    this.numOfPage = this.accountData.length % 10 + 1
+    this.changePage()
   },
   methods: {
     /**
@@ -91,11 +153,33 @@ export default {
         background: 'rgba(0, 0, 0, 0.7)'
       })
     },
-    handleEdit (index, row) {
-      console.log(index, row)
+    handleClicked (index, row, typeButton) {
+      this.$confirm('Do you want to ' + String(typeButton).toLowerCase() + ' ' + row.fullname + ' ?', 'Warning', {
+        confirmButtonText: 'Yes',
+        cancelButtonText: 'No',
+        type: 'warning'
+      }).then(() => {
+        row.status = typeButton
+        this.showMessage(typeButton + ' user Successful !!!', 'success')
+      })
     },
-    handleDelete (index, row) {
-      console.log(index, row)
+    handleInactiveClicked (index, row) {
+      row.status = 'Inactive'
+    },
+    changePage () {
+      let firstIndex = (this.currentPage - 1) * this.pageSize
+      let lastIndex = (this.currentPage * this.pageSize - 1)
+      this.subAccountData = this.accountData.filter((item, index) => {
+        return index >= firstIndex && index <= lastIndex
+      })
+    },
+    changeTextSearch () {
+      this.searchList = this.accountData.filter(data => !this.search || data.fullname.toLowerCase().includes(this.search.toLowerCase()))
+      let firstIndex = (this.currentPage - 1) * this.pageSize
+      let lastIndex = (this.currentPage * this.pageSize - 1)
+      this.subAccountData = this.searchList.filter((item, index) => {
+        return index >= firstIndex && index <= lastIndex
+      })
     },
     /**
      * Close Loader
