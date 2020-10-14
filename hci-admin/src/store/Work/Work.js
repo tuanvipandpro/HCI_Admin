1ea1a27c67b64b5a24efa7858f65093e7e39ce58
TuanLM <tuanvipandpro@gmail.com>
2020-10-14 11:09:58 +0700

    Add Api

--- hci-admin/src/store/Work/Work.js
+++ hci-admin/src/store/Work/Work.js
@@ -28,6 +28,19 @@ const work = {
           resolve(res)
         }).catch(e => reject(e))
       })
+    },
+    /**
+     * Delete work by id
+     * @param context
+     * @param params
+     */
+    _deleteWork (context, params) {
+      let url = 'https://tuanlm-hr.herokuapp.com/api/work/delete-work/' + params.id
+      return new Promise((resolve, reject) => {
+        axios.delete(url)
+          .then(res => resolve(res))
+          .catch(e => reject(e))
+      })
     }
   }
 }
