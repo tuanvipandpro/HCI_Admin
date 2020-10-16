28bd5212126e649dcbd8c2d0faf981f55f980de7
DatVTP <vanthanhphuongdat1309@gmai.com>
2020-10-16 16:40:43 +0700

    Create Account code

---
+++ hci-admin/src/store/Account/NewAccount.js
@@ -0,0 +1,45 @@
+import axios from 'axios'
+
+const createAccount = {
+  namespaced: true,
+  state: {
+    _accountFormData: {}
+  },
+  getters: {
+
+  },
+  mutations: {
+    /**
+     * setter for _accountFormData
+     */
+    _setAccountFormData (state, _accountFormData) {
+      state._accountFormData = _accountFormData
+    }
+  },
+  actions: {
+    /**
+     * create a notification function
+     */
+    _createAccount (context) {
+      let url = 'https://tuanlm-hr.herokuapp.com/api/account/insert-account'
+      return new Promise((resolve, reject) => {
+        axios.post(url,
+          {
+            username: context.state._accountFormData.username,
+            password: context.state._accountFormData.password,
+            employeeId: context.state._accountFormData.employeeId
+          },
+          {headers: {Authorization: sessionStorage.getItem('token')}}
+        )
+          .then((res) => {
+            resolve(res)
+          })
+          .catch((e) => {
+            reject(e)
+          })
+      })
+    }
+  }
+}
+
+export default createAccount
