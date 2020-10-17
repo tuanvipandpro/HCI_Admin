7497fe8c062a8ea9c75375324b8102391ff0e73e
TuanLM <tuanvipandpro@gmail.com>
2020-10-17 10:04:47 +0700

    Add Button Handle

--- hci-admin/src/components/Work/WorkAssign.vue
+++ hci-admin/src/components/Work/WorkAssign.vue
@@ -6,31 +6,33 @@
       </el-col>
       <el-col :offset="5" :span="19">
           <el-form
+            :model="formData"
+            :rules="formRules"
             style="margin-left: 1%"
-            ref="work-assign-form"
+            ref="assignForm"
             status-icon>
                 <h2>Phân công ca</h2>
-                <el-form-item label="Chọn cửa hàng" required>
+                <el-form-item label="Chọn cửa hàng" required prop="store">
                     <el-select style="width: 30%; margin-left: 0.1%" v-model="formData.store" placeholder="Chọn cửa hàng cần phân công">
                         <el-option label="Cửa hàng 1" value="1"/>
                         <el-option label="Cửa hàng 2" value="2"/>
                     </el-select>
                 </el-form-item>
-                <el-form-item label="Chọn ca làm" required>
+                <el-form-item label="Chọn ca làm" required prop="shift">
                     <el-select style="width: 30%; margin-left: 1.8%" v-model="formData.shift" placeholder="Chọn ca cần giao việc">
                         <el-option label="Ca Sáng" value="1"/>
                         <el-option label="Ca Chiều" value="2"/>
                     </el-select>
                 </el-form-item>
-                <el-form-item label="Chọn nhân viên" required>
+                <el-form-item label="Chọn nhân viên" required prop="employee">
                     <el-select style="width: 30%" v-model="formData.employee" placeholder="Chọn ca cần giao việc">
                         <el-option label="Nhân viên 1" value="1"/>
                         <el-option label="Nhân viên 2" value="2"/>
                     </el-select>
                 </el-form-item>
                 <el-form-item>
-                    <el-button type="primary">Create</el-button>
-                    <el-button>Cancel</el-button>
+                    <el-button type="primary" @click="submitForm">Xác nhận</el-button>
+                    <el-button @click="resetForm">Làm mới</el-button>
                 </el-form-item>
           </el-form>
       </el-col>
@@ -52,7 +54,19 @@ export default {
         store: '',
         shift: '',
         employee: ''
+      },
+      formRules: {
+        store: [
+          {required: true, message: 'Vui lòng chọn cửa hàng cần giao ca', trigger: 'blur'}
+        ],
+        shift: [
+          {required: true, message: 'Vui lòng chọn ca cần giao', trigger: 'blur'}
+        ],
+        employee: [
+          {required: true, message: 'Vui lòng chọn nhân viên cần giao', trigger: 'blur'}
+        ]
       }
+      // end
     }
   },
   computed: {
@@ -65,6 +79,28 @@ export default {
   },
   methods: {
     ...mapActions('workAssign', ['_getStoreList', '_getShiftList', '_getEmployeeList', '_assignWork']),
+    /**
+     * Submit Form
+     */
+    submitForm () {
+      this.$refs['assignForm'].validate(res => {
+        console.log(res)
+        if (res) {
+          alert('ok')
+          this.resetForm()
+        } else {
+          alert('not ok')
+        }
+      })
+    },
+    /**
+     * Reset Form
+     */
+    resetForm () {
+      this.formData.store = ''
+      this.formData.shift = ''
+      this.formData.employee = ''
+    },
     /**
      * Show Loader
      */
